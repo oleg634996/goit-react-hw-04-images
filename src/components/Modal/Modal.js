90d4831b85f32ae1b8ad65d45127973751580730
oleg634996 <olegkolodiy22@gmail.com>
2022-07-26 10:42:25 +0300

    исправил ДЗ

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -3,17 +3,18 @@ import { useEffect } from 'react';
 
 function Modal({ onClose, onOpenImg }) {
   useEffect(() => {
+    const handleKeyDown = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
     window.addEventListener('keydown', handleKeyDown);
-  });
 
-  useEffect(() => {
-    window.removeEventListener('keydown', handleKeyDown);
-  });
-  const handleKeyDown = event => {
-    if (event.code === 'Escape') {
-      onClose();
-    }
-  };
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [onClose]);
+
   return (
     <div
       className="overlay"
